eebfd912a22c1821680f88e0635e5a495fbf1fbe
Isaac Balintuma <ibalintuma34@gmail.com>
2025-02-13 06:51:27 +0800

    connected Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:ambulance_app/SplashPage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //WakelockPlus.enable(); // For Flutter v1.12.13 and later
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
